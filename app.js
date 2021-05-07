62c654e168a0a875c7e98d0b26e5bbcf7b45cf8f
DrBennemin <benjamin.krzis@gmail.com>
2021-05-07 19:27:35 +0200

    Prettify time in input fields

--- app.js
+++ app.js
@@ -1,50 +1,62 @@
 // Selectors
-const hours = document.querySelector("#hours")
-const minutes = document.querySelector("#minutes")
-const seconds = document.querySelector("#seconds")
-const timer = document.querySelector(".timer")
-const start = document.querySelector("#start")
-const pause = document.querySelector("#pause")
-const reset = document.querySelector("#reset")
+const hours = document.querySelector("#hours");
+const minutes = document.querySelector("#minutes");
+const seconds = document.querySelector("#seconds");
+const timer = document.querySelector(".timer");
+const start = document.querySelector("#start");
+const pause = document.querySelector("#pause");
+const reset = document.querySelector("#reset");
 
 const msSeconds = 1000;
-const msMinutes = msSeconds*60;
-const msHours = msMinutes*60;
+const msMinutes = msSeconds * 60;
+const msHours = msMinutes * 60;
 
 let time = 0;
 
 // Events
 timer.addEventListener("change", () => {
-    setTime();
-    console.log(time);
-})
+  prettifyTime();
+  setTime();
+  console.log(time);
+});
 
 start.addEventListener("click", () => {
-
-    start.disabled = true
-    let counter = setInterval(() => {
-        if (time >= msSeconds){
-
-            time = time-msSeconds;
-            console.log(time)
-        } else {
-            console.log("Ende");
-            clearInterval(counter)
-        }
-  }, msSeconds) 
-})
+  start.disabled = true;
+  let counter = setInterval(() => {
+    if (time >= msSeconds) {
+      time = time - msSeconds;
+      console.log(time);
+    } else {
+      console.log("Ende");
+      clearInterval(counter);
+    }
+  }, msSeconds);
+});
 pause.addEventListener("click", () => {
-    console.log("pause");
-})
+  console.log("pause");
+});
 reset.addEventListener("click", () => {
-    location.reload();
-})
+  location.reload();
+});
 
 // Functions
-function setTime(){
-    time = hours.value*msHours+minutes.value*msMinutes+seconds.value*msSeconds;
+function setTime() {
+  time =
+    hours.value * msHours +
+    minutes.value * msMinutes +
+    seconds.value * msSeconds;
 }
 
-function startTimer(){
+function startTimer() {}
 
+function timePrettifier(key) {
+  if (key.value < 10) {
+    key.value = `0${key.value}`.slice(-2);
+  }
+}
+
+function prettifyTime() {
+  timePrettifier(hours);
+  timePrettifier(minutes);
+  timePrettifier(seconds);
 }
